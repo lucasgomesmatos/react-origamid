7570d878b68ed4be5587d1ca638475853e3ec5d9
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-07-04 08:57:12 -0300

    modules

--- react-completo/modules-javascript/script.js
+++ react-completo/modules-javascript/script.js
@@ -1,45 +1,4 @@
-function handleMouseMove({clientX, clientY}) {
-  console.log(clientX, clientY)
-}
+import {areaQuadrado, perimetroQuadrado} from "./modules/perimetroQuadrado.js";
 
-// document.documentElement.addEventListener("mousemove", handleMouseMove);
-
-const frutas = ["banana", "maçã", "uva"];
-
-const frutaToUpperCase = frutas.map((fruta) => {
-  return fruta.toUpperCase();
-});
-
-console.log(frutaToUpperCase);
-
-
-const items = document.querySelectorAll("li");
-
-const elements = [...items].map(item => {
-  return item
-})
-
-
-
-console.log(elements)
-
-class Transporte {
-  constructor() {
-    this.transporte = true;
-  }
-}
-
-class Carro extends Transporte {
-  constructor(cor, rodas) {
-    super();
-    this.cor = cor;
-    this.rodas = rodas;
-  }
-}
-
-
-const carro = new Carro("preto", 4);
-const cloneCarro = {...carro}
-
-console.log(carro);
-console.log(cloneCarro);
+console.log(areaQuadrado(4))
+console.log(perimetroQuadrado(4))
