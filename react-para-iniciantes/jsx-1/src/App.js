48ca0742c7c0475e5f6b6a0b1fee2cd744a00e2b
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-07-06 11:58:47 -0300

    JSX 1, javascript

--- react-para-iniciantes/jsx-1/src/App.js
+++ react-para-iniciantes/jsx-1/src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
 
 const App = () => {
+  function mostrarNome() {
+    return "Lucas";
+  }
+
+  const nome = mostrarNome();
+
+  const carro = {
+    nome: "Hilux",
+    portas: 5,
+    abrir() {
+      return "Abriu a porta";
+    },
+  };
+
+  const estiloP = {
+    color: "tomato",
+    fontSize: "2rem",
+  };
+
   return (
     <div>
-      <a href="https://github.com/lucasgomesmatos">GitHub</a>
+      <a className="ativo" href="https://github.com/lucasgomesmatos">
+        {nome === "Lucas" ? "Lucas" : "Github"}
+      </a>
+      <p>{new Date().getFullYear()}</p>
+
+      <p style={estiloP}>{carro.nome}</p>
+      <p>{carro.portas}</p>
+      <p>{carro.abrir()}</p>
     </div>
   );
-}
+};
 
 export default App;
