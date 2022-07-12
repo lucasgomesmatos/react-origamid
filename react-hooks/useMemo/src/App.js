9d719f9d0dcfec9bebe654846703edb7713b33a9
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-07-12 20:19:48 -0300

    useMemo e useCallback

---
+++ react-hooks/useMemo/src/App.js
@@ -0,0 +1,34 @@
+import React, { useMemo, useState } from "react";
+
+function operacaoLenta() {
+  let c;
+  for (let i = 0; i < 100000000; i++) {
+    c = i + i / 10;
+  }
+  return c;
+}
+
+const App = () => {
+  const [contar, setContar] = useState(0);
+
+  // function handleClick() {
+  //   setContar(contar + 1);
+  // }
+  // const valor = useMemo(() => {
+  //   const localItem = window.localStorage.getItem("produto");
+  //   console.log("Aconteceu o memo");
+  //   return localItem;
+  // }, []);
+
+  const t1 = performance.now()
+  const valor = useMemo(() => {
+    return operacaoLenta()
+  }, [])
+
+  console.log(performance.now() - t1);
+  console.log(valor);
+
+  return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
+};
+
+export default App;
