bc16b642a2c18b67138cf054d6dd34932378d086
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-07-26 21:08:46 -0300

    desafio form

---
+++ formularios/desafio-formulario/src/Form/Radio.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Radio = ({ active, pergunta, options, id, value, onChange }) => {
+  if (!active) return false;
+  return (
+    <fieldset
+      style={{
+        padding: '2rem',
+        marginBottom: '1rem',
+        border: '2px solid #eee',
+      }}
+    >
+      <legend style={{ fontWeight: 'bold' }}>{pergunta}</legend>
+      {options.map((option) => (
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
+          <input
+            type="radio"
+            id={id}
+            checked={value === option}
+            value={option}
+            onChange={onChange}
+          />
+          {option}
+        </label>
+      ))}
+    </fieldset>
+  );
+};
+
+export default Radio;
