af087ca404b737c8349741ac0193e034548b8073
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-07-30 22:08:13 -0300

    slides

--- css/animacoes/src/App.js
+++ css/animacoes/src/App.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 import Produto from './Produto';
+import Slide from './Slide';
 import './styles/style.sass';
 
 const App = () => {
   const [ativo, setAtivo] = useState(false);
 
+  const slides = [
+    {
+      id: 'slide1',
+      text: 'Slide 1',
+    },
+    {
+      id: 'slide2',
+      text: 'Slide 2',
+    },
+    {
+      id: 'slide3',
+      text: 'Slide 3',
+    },
+    {
+      id: 'slide4',
+      text: 'Slide 4',
+    },
+  ];
+
   return (
     <div>
-      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
-      {ativo && <Produto />}
+      <Slide slides={slides} />
     </div>
   );
 };
---
+++ css/animacoes/src/Slide.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './styles/slide.module.sass';
+
+const Slide = ({ slides }) => {
+  const [active, setActive] = useState(0);
+  const [position, setPosition] = useState(0);
+  const contentRef = useRef();
+
+  useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
+    setPosition(-(width * active));
+  }, [active]);
+
+  function slidePrev() {
+    if (active > 0) setActive(active - 1);
+  }
+  function slideNext() {
+    if (active < slides.length - 1) setActive(active + 1);
+  }
+
+  return (
+    <section className={styles.container}>
+      <div
+        ref={contentRef}
+        className={styles.content}
+        style={{ transform: `translateX(${position}px)` }}
+      >
+        {slides.map((slide) => (
+          <div key={slide.id} className={styles.item}>
+            {slide.text}
+          </div>
+        ))}
+      </div>
+      <nav className={styles.nav}>
+        <button onClick={slidePrev}>Anterior</button>
+        <button onClick={slideNext}>Próxima</button>
+      </nav>
+    </section>
+  );
+};
+
+export default Slide;
